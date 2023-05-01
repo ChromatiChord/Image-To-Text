51f3a2ac4510605b66542d44668de4bfd4b7e187
z5204798 <callum.ward@student.unsw.edu.au>
2023-05-01 13:10:33 +1000

    CHORE: Added image directory creation for fresh pull usage

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from modules.grab_image import grab_image
 import config
+import os
 
 if __name__ == "__main__":
     image_directory = config.IMAGE_DIRECTORY
+    if not os.path.exists(image_directory):
+      os.makedirs(image_directory)
+
     print("Press Ctrl + Shift + PrintScreen to capture a snapshot. Then press Enter to save the snapshot and read the text.")
     
     while True:
